478c3729511ccf857dd9bf9ffafd243ecbb703f9
Amtul Ali <amtul.ali1983@gmail.com>
2023-07-10 21:41:39 -0400

    updated

--- Lab.NodeBasics/fileA.js
+++ Lab.NodeBasics/fileA.js
@@ -16,21 +16,21 @@
 
 //Snippet 2......................
 
-// const dogs = 'I love dogs!';
-// const cats = 'I love cats!';
-
-// module.exports = {
-//   dogs: dogs,
-//   cats: cats,
-// };
-
-// console.log(dogs);
-
-//Snippet 3........................
 const dogs = 'I love dogs!';
 const cats = 'I love cats!';
 
-module.exports = dogs;
-module.exports = cats;
+module.exports = {
+  dogs: dogs,
+  cats: cats,
+};
 
-console.log(dogs);
+console.log(module.exports);
+
+//Snippet 3........................
+// const dogs = 'I love dogs!';
+// const cats = 'I love cats!';
+
+// module.exports = dogs;
+// module.exports = cats;
+
+// console.log(module.exports);
